32b7d396bc83c395f4e2bd15d775be173ddd7bf7
LordTali <162058411+LordTali@users.noreply.github.com>
2024-05-06 19:29:38 +0200

    main.py

--- main.py
+++ main.py
@@ -12,8 +12,3 @@ import sys
 
 def Textpause():
     time.sleep(0.8)
-
-
-    #schalflaeche_dialog = tk.Button(fenster_dialog, text=dialogoption, bg="white", fg="black", font=("Helvetica", 12), command=lambda option = dialogoption: antworten(option))
-    #schalflaeche_dialog.pack(side=tk.TOP, padx=10, pady=5)
-
